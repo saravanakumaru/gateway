06e3af6c426a119b53cddeb7746b90eb774d72b2
Saravana <saravana.kumar@rakuten.com>
2018-08-01 17:23:49 +0800

    config server changes

--- src/main/java/com/test/zuul/gateway/GatewayApplication.java
+++ src/main/java/com/test/zuul/gateway/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.test.zuul.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import com.test.zuul.gateway.filters.TestFilter;
 
 @EnableZuulProxy
+@EnableConfigServer
 @SpringBootApplication
 public class GatewayApplication {
 
